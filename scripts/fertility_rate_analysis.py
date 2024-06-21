7d23bf3e8f55ed81013869ccaf2b504b0feed40d
Ian Dover <iantdover@gmail.com>
2024-06-21 04:20:41 +0000

    added new file to project

---
+++ scripts/fertility_rate_analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+import matplotlib.ticker as ticker
+import seaborn as sns
+
+
+if __name__ == "__main__":
+    print(None)
